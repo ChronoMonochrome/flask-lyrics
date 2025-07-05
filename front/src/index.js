047f86d98ac12f375ca325f8439e5f80d4859455
Victor Shilin <chrono.monochrome@gmail.com>
2025-07-05 20:41:07 +0300

    react fix

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,19 @@
+// front/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // Optional: for global styles
-import App from './App'; // Import your main App component
-// import reportWebVitals from './reportWebVitals'; // REMOVE OR COMMENT OUT THIS LINE
+import ReactDOM from 'react-dom/client'; // Use createRoot for React 18+
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter here
+import './index.css'; // Your global CSS
+import App from './App'; // Your main App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*
+      IMPORTANT: Wrap your entire App component (or the part that uses React Router)
+      with BrowserRouter.
+    */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(); // REMOVE OR COMMENT OUT THIS LINE
