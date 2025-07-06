67322114f2a7865f9d577b08529f28ab930e3901
Victor Shilin <chrono.monochrome@gmail.com>
2025-07-06 07:24:20 +0300

    Fix registration

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = Column(String(80), unique=True, nullable=False)
-    password_hash = Column(String(128), nullable=False)
+    password_hash = Column(String(512), nullable=False)
     email = Column(String(120), unique=True, nullable=True)
     xp = Column(Integer, default=0, nullable=False)
     level = Column(Integer, default=1, nullable=False)
